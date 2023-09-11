f9fdc5fe40f2161af7f2dc9189f7ab926e3a39b5
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-09-11 10:48:42 -0700

    update download button

--- src/navv/gui/app.py
+++ src/navv/gui/app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+import logging
+import os
 
-from navv.message_handler import warning_msg
+from flask import Flask, render_template, send_from_directory
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    for file in os.listdir(os.getcwd()):
+        if file.endswith(".pcap"):
+            logger.info(f"Found pcap file: {file}")
+            break
     return render_template("index.html")
+
+
+@app.route("/download")
+def download():
+    """Download the network analysis excel file."""
+    filename = "test-customer_network_analysis.xlsx"
+    current_path = os.getcwd()
+
+    if not os.path.isfile(os.path.join(current_path, filename)):
+        logger.error(f"File {filename} not found in {current_path}")
+
+    return send_from_directory(
+        current_path,
+        filename,
+        as_attachment=True,
+    )
